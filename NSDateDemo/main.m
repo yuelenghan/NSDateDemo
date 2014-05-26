e08536f16ff1899a5705c45b82ce9177293fbad0
yuelenghan <lh_ztt@qq.com>
2014-05-26 09:12:14 +0800

    Initial Commit

---
+++ NSDateDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSDateDemo
+//
+//  Created by 李鹤 on 14-5-26.
+//  Copyright (c) 2014年 国华天能. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
